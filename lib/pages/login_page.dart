d16bcabccc17ea3576b68e78807de7df90405c94
ian otieno <ianotieno23@gmail.com>
2025-11-11 11:53:56 +0300

    update

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,14 +10,16 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   GlobalKey<FormState> _loginFormKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: const Text('Login Page')),
-      body: SafeArea(child: _buildUI()),
+      body: SafeArea(
+        
+       child: _buildUI()
+        ),
     );
   }
 
@@ -28,96 +30,92 @@ class _LoginPageState extends State<LoginPage> {
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
-       children: [
-        _title(),
-        _loginForm(),
-      ],
-        ),
-    );
-  }
-  Widget _title(){
-    return const Text(
-      'Welcome to Recipe App',
-      style: TextStyle(
-        fontSize: 24,
-
-        fontWeight: FontWeight.bold,
+        children: [_title(), _loginForm()],
       ),
     );
   }
-  Widget _loginForm(){
-    return  SizedBox(
-      width: MediaQuery.sizeOf(context).width * 0.90,
-      height: MediaQuery.sizeOf(context).height * 0.30,
-      child: Form(
-      key: _loginFormKey,  
-      child: Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          TextFormField(
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your username';
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              labelText: 'Username',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-            
-          ),
-          const SizedBox(height: 20),
-          TextFormField(
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your password';
-              }
-              return null;
-            },
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          _loginButton(),
-          const SizedBox(height: 20),
-          _forgotPasswordButton(),
-        
-        ],
-      )),
-    );
 
+  Widget _title() {
+    return const Text(
+      'Welcome to Recipe App',
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    );
   }
 
-  Widget _loginButton(){
+  Widget _loginForm() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.90,
+     
+      child: Form(
+        key: _loginFormKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your username';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            _loginButton(),
+            const SizedBox(height: 20),
+            _forgotPasswordButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
     return SizedBox(
       width: MediaQuery.sizeOf(context).width * 0.60,
-      child:ElevatedButton(
-      onPressed: () {
-        if (_loginFormKey.currentState?.validate() ?? false) {
-          // Process login
-        }
-      },
-      child: const Text('Login'),
-    ));
+      child: ElevatedButton(
+        onPressed: () {
+          if (_loginFormKey.currentState?.validate() ?? false) {
+            // Process login
+          }
+        },
+        child: const Text('Login'),
+      ),
+    );
   }
-  Widget _forgotPasswordButton(){
+
+  Widget _forgotPasswordButton() {
     return SizedBox(
       width: MediaQuery.sizeOf(context).width * 0.60,
       child: TextButton(
-      onPressed: () {
-        // Handle forgot password logic here
-      },
-      child: const Text('Forgot Password?'),
-    ));
+        onPressed: () {
+          // Handle forgot password logic here
+        },
+        child: const Text('Forgot Password?'),
+      ),
+    );
   }
 }
