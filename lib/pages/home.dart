5d4b9475bd79acc108ff16dea3d2eb03006c12bc
ian otieno <ianotieno23@gmail.com>
2025-11-12 16:53:09 +0300

    update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,19 +11,43 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: const Text('Home Page')),
+      appBar: AppBar(centerTitle: true, title: const Text('Recipe Book')),
       body: SafeArea(child: _buildUI()),
     );
   }
 
   Widget _buildUI() {
+    return Container(child: 
+    Column(children: [_recipeTypeButtons()]));
+  }
+
+  Widget _recipeTypeButtons() {
     return SizedBox(
-      width: MediaQuery.sizeOf(context).width,
-      child: const Center(
-        child: Text(
-          'Welcome to the Home Page!',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+      height: MediaQuery.sizeOf(context).height * 0.05,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+            child: FilledButton(onPressed: (){}, 
+            child: const Text('🥕 Snacks')),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+            child: FilledButton(onPressed: (){}, 
+            child: const Text('🍳 Brdeakfast')),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+            child: FilledButton(onPressed: (){}, 
+            child: const Text('🥩 lunch')),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+            child: FilledButton(onPressed: (){}, 
+            child: const Text('🍗 Dinner')),
+          )
+        ],
       ),
     );
   }
