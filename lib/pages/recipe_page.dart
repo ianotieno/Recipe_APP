391e75551cff9f0a8c0d4affd39b2e0b08c6da3b
ian otieno <ianotieno23@gmail.com>
2025-11-14 22:45:43 +0300

    update

--- lib/pages/recipe_page.dart
+++ lib/pages/recipe_page.dart
@@ -4,39 +4,59 @@ import 'package:recepies/models/recipe.dart';
 class RecipePage extends StatelessWidget {
   final Recipe recipe;
 
-  RecipePage({
-    required this.recipe,
-  });
+  RecipePage({required this.recipe});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, 
-      title: const Text('Recipe Details')),
-      body: SafeArea(
-      
-        child: _buildUI(context),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(centerTitle: true, title: const Text('Recipe Details')),
+      body: SafeArea(child: _buildUI(context)),
+    );
+  }
+
+  Widget _buildUI(BuildContext context) {
+    return Column(children: [_recipeImage(context), _recipeDetails(context)]);
+  }
+
+  _recipeImage(BuildContext context) {
+    return Container(
+      width: MediaQuery.sizeOf(context).width,
+      height: MediaQuery.sizeOf(context).height * 0.40,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(recipe.image),
+          fit: BoxFit.cover,
+        ),
       ),
     );
-   
   }
-   Widget _buildUI(BuildContext context) {
-      return  Column(
-        children: [
-          _recipeImage( context),
-        ],
-      );
-    }
-    _recipeImage(BuildContext context) {
-      return Container(
-        width: MediaQuery.sizeOf(context).width,
-        height: MediaQuery.sizeOf(context).height * 0.40,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(recipe.image),
-            fit: BoxFit.cover,
-          ),
-        ),
-      );
-    }
+
+  _recipeDetails(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: MediaQuery.sizeOf(context).width,
+      padding: const EdgeInsets.all(16.0),
+
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+       children: [
+            Text("${recipe.cuisine}, ${recipe.difficulty}",
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w300,
+            ) 
+            ),
+            Text(recipe.name,
+            style: const TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ) 
+            ),
+       ],
+      ),
+    );
+  }
 }
